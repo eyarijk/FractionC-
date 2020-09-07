4c8ede81eb5cefd193aa837f1d2874c4da9004a5
Yaroslav Ilnitskiy <eyarij@gmail.com>
2020-09-07 09:32:49 +0300

    fraction

---
+++ lab2/Fraction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace lab2
+{
+    public class Fraction
+    {
+        private double numerator;
+        private double denominator;
+
+        public Fraction(double numerator, double denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new Exception("Denominator must be <> 0");
+            }
+
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public double GetNumerator()
+        {
+            return this.numerator;
+        }
+
+        public double GetDenominator()
+        {
+            return this.denominator;
+        }
+
+        public double GetSum()
+        {
+            return this.numerator + this.denominator;
+        }
+
+        public double GetSubtraction()
+        {
+            return this.numerator - this.denominator;
+        }
+
+        public double GetDivision()
+        {
+            return this.numerator / this.denominator;
+        }
+
+        public double GetMultiplication()
+        {
+            return this.numerator * this.denominator;
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Fraction fraction = new Fraction(10, 2);
+
+            Console.WriteLine("Sum = " + fraction.GetSum());
+            Console.WriteLine("Division = " + fraction.GetDivision());
+            Console.WriteLine("Multiplication = " + fraction.GetMultiplication());
+            Console.WriteLine("Subtraction = " + fraction.GetSubtraction());
+        }
+    }
+}
